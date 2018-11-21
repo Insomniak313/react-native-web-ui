c8678797149c05fa3cef0d5586a6b97be480e954
Fournie <afournie@finindev.com>
2018-11-21 14:06:33 +0100

    Premier commit des composants

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Button from './exports/Button';
+import LoadingPlaceholder from './exports/LoadingPlaceholder';
+import Switch from './exports/Switch';
+import TextField from './exports/TextField';
+import VictoryChart from './exports/VictoryChart';
+import TextFormGroup from './exports/TextFormGroup';
+
+const ReactNativeWebUI = {
+	Button,
+	LoadingPlaceholder,
+	Switch,
+	TextField,
+	VictoryChart,
+	TextFormGroup
+};
+
+export default ReactNativeWebUI;
